3ddca510d32812ed7df8d13c9037c8b7b9e44875
Ludovic <2012878@cegepmontpetit.ca>
2023-04-18 09:21:54 -0400

    FCT
    Ajout des fichier et écriture du README

---
+++ TP2 Ludovic Blanchette2/Tp2/TP2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TP2.Models;
+
+namespace TP2.controller
+{
+    public class HomeController : Controller
+    {
+            private Database DB { get; set; }
+
+            public HomeController(Database db)
+            {
+                this.DB = db;
+            }
+        public ActionResult Index()
+        {
+            return View(DB.Parents.ToList());
+        }
+
+    }
+}
---
+++ TP2 Ludovic Blanchette2/Tp2/TP2/ViewModels/PageRechercheViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TP2.Models;
+namespace TP2.ViewModels
+{
+    public class PageRechercheViewModel
+    {
+        #region propriétés
+        public CritereRechercheViewModel Criteres { get; set; }
+        public List<Enfant> Resultat { get; set; }
+
+        #endregion
+    }
+}
