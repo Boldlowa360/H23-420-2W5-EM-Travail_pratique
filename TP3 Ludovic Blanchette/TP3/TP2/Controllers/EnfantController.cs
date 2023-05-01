0d6afba9d8022536c2e5c2dc8a0f06c6ba5ede3f
Ludovic <2012878@cegepmontpetit.ca>
2023-05-01 12:04:19 -0400

    FCT
    Critère de recherche

--- TP3 Ludovic Blanchette/TP3/TP2/Controllers/EnfantController.cs
+++ TP3 Ludovic Blanchette/TP3/TP2/Controllers/EnfantController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TP2.ViewModels;
 using TP2.Models;
+using Microsoft.AspNetCore.Http.Metadata;
 
 namespace TP2.Controllers
 {
@@ -59,34 +60,64 @@ namespace TP2.Controllers
             IEnumerable<Enfant> donnees = DB.Enfants;
 
 
-            //filtre par Favoris
-            //Oui
-            if (critere.Vedette != null)
+            //filtre par Favoris        
+            if (critere.Nom != null)
             {
-                if (critere.Vedette == "Oui")
-                {
-                    donnees = this.DB.Enfants.Where(h => h.Favoris == true);
-                }
-                //Non
-                if (critere.Vedette == "Non")
-                {
-                    donnees = this.DB.Enfants.Where(h => h.Favoris == false);
-                }
-                //peu importe
-                if (critere.Vedette == "Peu importe")
-                {//Filtre par mots clé
-                    if (critere.Nom != null)
-                    {
-                        donnees = this.DB.Enfants.Where(h => h.nom.ToUpper().Contains(critere.Nom.ToUpper()));
-                    }
-                    //filtre par niveau de difficulté
-                    if (critere.Difficulté == 1)
-                    {
-                        donnees = this.DB.Enfants.Where(h => h.difficulté == 1);
-                    }
-
-                }
+                donnees = donnees.Where(h => h.nom.ToUpper().Contains(critere.Nom.ToUpper()));
             }
+            //filtre par niveau de difficulté
+            if (critere.Difficulté == 1)
+            {
+                donnees = donnees.Where(h => h.difficulté >= 1);
+            }
+            //Oui
+            if (critere.Favoris == "Oui")
+            {
+                donnees = donnees.Where(h => h.Favoris == true);
+            }
+            //Non
+            if (critere.Favoris == "Non")
+            {
+                donnees = donnees.Where(h => h.Favoris == false);
+            }
+            //Tous
+            if (critere.Favoris == "Tous")
+            {
+
+            }
+            //Par niveau de difficulté
+            if (critere.Difficulté == 1)
+            {
+                donnees = donnees.Where(h => h.difficulté == 1);
+            }
+            if (critere.Difficulté == 2)
+            {
+                donnees = donnees.Where(h => h.difficulté == 2);
+            }
+            if (critere.Difficulté == 3)
+            {
+                donnees = donnees.Where(h => h.difficulté == 3);
+            }
+            //par role
+            // DPS
+            if (critere.Dps == true)
+            {
+                donnees = donnees.Where(h => h.IdParent == 1);
+            }
+            //Healer
+            if (critere.Soigneur == true)
+            {
+                donnees = donnees.Where(h => h.IdParent == 2);
+            }
+            //Tank
+            if (critere.Tank == true)
+            {
+                donnees = donnees.Where(h => h.IdParent == 3);
+            }
+            //Tous  
+            
+
+
 
 
             var model = new PageRechercheViewModel();
@@ -94,47 +125,6 @@ namespace TP2.Controllers
             model.Resultat = donnees.ToList();
 
             return View(nameof(Recherche), model);
-        }
-
-        [Route("/Enfant/Recherche/Filtrer")]
-        [Route("/enfant/Filtrer")]
-        [Route("/Filtrer")]
-        public IActionResult Filtrer(CritereRechercheViewModel critere)
-        {
-            IEnumerable<Enfant> donnees = DB.Enfants;
-
-            //Filtre par mots clé
-            if(critere.Nom != null)
-            {
-                donnees = this.DB.Enfants.Where(h => h.nom.ToUpper().Contains(critere.Nom.ToUpper()));
-            }
-            //filtre par niveau de difficulté
-            if(critere.Difficulté != null)
-            {
-                donnees = this.DB.Enfants.Where(h => h.difficulté == critere.Difficulté);
-            }
-            //filtre par Favoris
-            //Oui
-            if(critere.Vedette == "Oui")
-            {
-                donnees = this.DB.Enfants.Where(h => h.Favoris == true);
-            }
-            //Non
-            if(critere.Vedette == "Non")
-            {
-                donnees = this.DB.Enfants.Where(h => h.Favoris == false);
-            }
-            //peu importe
-            if(critere.Vedette == "Peu importe")
-            {
-                donnees = this.DB.Enfants.ToList();
-            }
-
-            var model = new PageRechercheViewModel();
-            model.Criteres = new CritereRechercheViewModel();
-            model.Resultat = donnees.ToList();
-
-            return View(nameof(Recherche),model);
         }
 
     }
